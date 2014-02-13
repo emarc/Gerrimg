d4c600f355ea4a03c983e85e84192024f14961ff
Marc Englund <marc@vaadin.com>
2014-02-13 09:39:07 +0200

    Minor adjustments, bookmarklet page added.

--- gerrimg.js
+++ gerrimg.js
@@ -1,17 +1,17 @@
 /* Gerrit image compare helper by Marc Englund. Inspired by TwoFace and js-imagediff. */
 (function () {
     "use strict";
-	var self = {
-		hs : 2,
-		width : 0,
-		height : 0,
-		ctx : null,
-		divide : 1,
-		dragstart : false,
-		before : null,
-		after : null,
-    
-        draw : function () {
+    var self = {
+        hs: 2,
+        width: 0,
+        height: 0,
+        ctx: null,
+        divide: 1,
+        dragstart: false,
+        before: null,
+        after: null,
+
+        draw: function () {
             this.ctx.globalCompositeOperation = "source-over";
             this.drawImages();
             this.drawHandle();
@@ -43,91 +43,93 @@
             this.dragstart = false;
             this.draw();
         },
-        dblclick : function (event) {
-            var c = document.createElement('canvas');
-            var img = self.before;
+        dblclick: function (event) {
+            var c = document.createElement('canvas'),
+                img = self.before;
             c.setAttribute('width', self.width);
             c.setAttribute('height', self.height);
             var ctx = c.getContext('2d');
-            ctx.drawImage(img,0,0);
-            var bd = ctx.getImageData(0,0,self.width,self.height);
+            ctx.drawImage(img, 0, 0);
+            var bd = ctx.getImageData(0, 0, self.width, self.height);
 
-            ctx.clearRect(0,0,self.width,self.height);
+            ctx.clearRect(0, 0, self.width, self.height);
             img = self.after;
-            ctx.drawImage(img,0,0);
-            var ad = ctx.getImageData(0,0,self.width,self.height);
-            
-            var diff = self.diff(bd,ad);
-            self.ctx.clearRect(0,0,self.width,self.height);
-            self.ctx.putImageData(diff,0,0);
+            ctx.drawImage(img, 0, 0);
+            var ad = ctx.getImageData(0, 0, self.width, self.height);
+
+            var diff = self.diff(bd, ad);
+            self.ctx.clearRect(0, 0, self.width, self.height);
+            self.ctx.putImageData(diff, 0, 0);
         },
         diff: function (a, b) {
-            var
-                height  = a.height,
-                width   = a.width,
-                c       = getImageData(width, height), 
-                aData   = a.data,
-                bData   = b.data,
-                cData   = c.data,
-                length  = cData.length,
-                row, column,
-                i, j, k, v;
-
+            var height = a.height,
+                width = a.width,
+                c = getImageData(width, height),
+                aData = a.data,
+                bData = b.data,
+                cData = c.data,
+                length = cData.length,
+                row,
+                column,
+                i,
+                j,
+                k,
+                v,
+                columnOffset,
+                rowOffset;
 
             for (i = 0; i < length; i += 4) {
-                if (aData[i] !== bData[i] 
-                    || aData[i+1] !== bData[i+1] 
-                    || aData[i+2] !== bData[i+2] 
-                    || aData[i+3] !== bData[i+3]) {
-                    cData[i] = cData[i+1] = cData[i+2] = 0;
-                    cData[i+3] = 255;
+                if (aData[i] !== bData[i] || aData[i + 1] !== bData[i + 1] || aData[i + 2] !== bData[i + 2] || aData[i + 3] !== bData[i + 3]) {
+                    cData[i] = cData[i + 1] = cData[i + 2] = 0;
+                    cData[i + 3] = 255;
                 }
             }
 
 
-        // Helpers
-        function getImageData (width, height) {
-            var canvas = document.createElement('canvas');
-            var context = canvas.getContext('2d');
-            canvas.width = width;
-            canvas.height = height;
-            context.clearRect(0, 0, width, height);
-            return context.createImageData(width, height);
-        }
-        function offsets (imageData) {
-          rowOffset = Math.floor((height - imageData.height) / 2);
-          columnOffset = Math.floor((width - imageData.width) / 2);
+            // Helpers
+            function getImageData(width, height) {
+                var canvas = document.createElement('canvas');
+                var context = canvas.getContext('2d');
+                canvas.width = width;
+                canvas.height = height;
+                context.clearRect(0, 0, width, height);
+                return context.createImageData(width, height);
+            }
+
+            function offsets(imageData) {
+                rowOffset = Math.floor((height - imageData.height) / 2);
+                columnOffset = Math.floor((width - imageData.width) / 2);
+            }
+
+            return c;
         }
 
-        return c;
-      }
+    };
 
-	};
-	
-	// find images
-	var els = document.getElementsByClassName("fileLineDELETE");
-	if (els.length && els[0].firstChild.tagName === "IMG") {
-		self.before = els[0].firstChild;
-	}
-	els = document.getElementsByClassName("fileLineINSERT");
-	if (els.length && els[0].firstChild.tagName === "IMG") {
-		self.after = els[0].firstChild;
-	}
-	if (self.before === null || self.after === null) {
-		return;
-	}
-	// set size
-	self.width = Math.max(self.before.width, self.after.width);
-	self.height = Math.max(self.before.height, self.after.height);
-	// create canvas
-	var canvas = document.createElement('canvas');
-	canvas.setAttribute('width', self.width);
+    // find images
+    var els = document.getElementsByClassName("fileLineDELETE");
+    if (els.length && els[0].firstChild.tagName === "IMG") {
+        self.before = els[0].firstChild;
+    }
+    els = document.getElementsByClassName("fileLineINSERT");
+    if (els.length && els[0].firstChild.tagName === "IMG") {
+        self.after = els[0].firstChild;
+    }
+    if (self.before === null || self.after === null) {
+        return;
+    }
+    // set size
+    self.width = Math.max(self.before.width, self.after.width);
+    self.height = Math.max(self.before.height, self.after.height);
+    // create canvas
+    var canvas = document.createElement('canvas');
+    canvas.setAttribute('width', self.width);
     canvas.setAttribute('height', self.height);
     var container = self.before.parentNode;
     container.removeChild(self.before);
     container.appendChild(canvas);
-    
-	self.ctx = canvas.getContext('2d');
+
+    self.ctx = canvas.getContext('2d');
 
     function handler(ev) {
         if (ev.layerX || ev.layerX === 0) { // Firefox
@@ -147,7 +149,7 @@
     canvas.addEventListener('mouseup', handler, false);
     canvas.addEventListener('dblclick', handler, false);
 
-    
+
     self.draw();
 
 }());
